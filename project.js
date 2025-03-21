ee99e8402d9484f2f7313cf0a547596b8e5bea42
Bontori1987 <92803883+Bontori1987@users.noreply.github.com>
2025-03-21 11:48:08 +0700

    Update project.js
    update for image and about in pages folder

--- project.js
+++ project.js
@@ -6,8 +6,10 @@ const route = (event) => {
 };
 
 const routes = {
-    "/reel-genius/about.html": "/reel-genius/about.html",
-    "/reel-genius/images.html": "/reel-genius/images.html",
+    // "/reel-genius/about.html": "/reel-genius/about.html",
+    // "/reel-genius/images.html": "/reel-genius/images.html",
+     "/reel-genius/pages/about.html": "/reel-genius/pages/about.html",
+     "/reel-genius/pages/images.html": "/reel-genius/pages/images.html",
     "/reel-genius/pages/videos.html": "/reel-genius/pages/videos.html",
 };
 
