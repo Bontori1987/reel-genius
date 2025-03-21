0ebab293d8e4b748b8c131669f85a6c677b7a3dd
Bontori1987 <92803883+Bontori1987@users.noreply.github.com>
2025-03-21 11:27:04 +0700

    Update project.js
    change the new one because the previous is error

--- project.js
+++ project.js
@@ -1,19 +1,9 @@
-// const route = (event) => {
-//     event = event || window.event;
-//     event.preventDefault();
-//     window.history.pushState({}, "", event.target.href);
-//     handleLocation();
-// };
 const route = (event) => {
     event = event || window.event;
-    event.preventDefault();
     console.log("Routing to:", event.target.href);
     window.history.pushState({}, "", event.target.href);
     handleLocation();
 };
-
-console.log(document.getElementById("main-page").innerHTML);
-
 
 const routes = {
     "/reel-genius/about": "/reel-genius/about.html",
@@ -25,32 +15,15 @@ const handleLocation = async () => {
 
     console.log("pathname:", path);
     console.log("Fetching route:", route);
+    console.log("Routes:",route);
     
-    // if(route) {
-    //     const html = await fetch(route).then((data) => data.text());
-    //     document.getElementById("main-page").innerHTML = html;
-    // } else {
-    //     console.log('NOT OK')
-    // }
-
-    
-    const handleLocation = async () => {
-        const path = window.location.pathname;
-        const route = routes[path] || routes[404];
-    
-        console.log("pathname:", path);
-        console.log("Fetching route:", route);
-    
-        if (route) {
-            try {
-                const html = await fetch(route).then((res) => res.text());
-                document.getElementById("main-page").innerHTML = html;
-            } catch (error) {
-                console.error("Failed to fetch:", error);
-            }
-        }
-    };
-    
+    if(route) {
+        const html = await fetch(route).then((respond) => respond.text());
+        document.getElementById("main-page").innerHTML = html;
+        console.log(document.getElementById("main-page").innerHTML);
+    } else {
+        console.log('NOT OK')
+    }
 };
 
 window.onpopstate = handleLocation;
